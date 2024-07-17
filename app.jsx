72960eec138f9dc7f0fa92645f001b15aae2ec1f
Papa Mamadou Mbaye <mistermbaye@MacBook-Pro-de-Papa.local>
2024-07-17 10:10:10 +0200

    suppression

--- app.jsx
+++ app.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Location from './pages/Location';
-import Error from './pages/Error';
+import Header from 'projetkasa/src/Components/Header.jsx';
+import Footer from 'projetkasa/src/Components/Footer.jsx';
 
-function App() {
-  const basename = import.meta.env.MODE === 'production' ? '/kasa' : '';
 
-  return (
-    <Router basename={basename}>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/location/:id" element={<Location />}
